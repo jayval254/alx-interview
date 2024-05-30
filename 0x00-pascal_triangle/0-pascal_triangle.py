02d94d5bc9991f9e4c744efaa60c66812b08729e
jayval254 <valariekimuge1@gmail.com>
2024-05-30 15:52:56 +0000

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,10 +8,10 @@ def pascal_triangle(n):
     triangle = [[1]]  # Initialize the triangle with the first row
     for i in range(1, n):
 	# Calculate the value based on the two values above it
-        row = [1]
+        temp = [1]
         for j in range(len(triangle[i - 1]) - 1):
             curr = triangle[i - 1]
-            row.append(triangle[i - 1][j] + triangle[i - 1][j + 1])
-        row.append(1)
-        triangle.append(row)
+            temp.append(triangle[i - 1][j] + triangle[i - 1][j + 1])
+        temp.append(1)
+        triangle.append(temp)
     return triangle
