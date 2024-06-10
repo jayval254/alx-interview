462d98d4ac02b05a9a71382d5ab2a9640fcd3e08
Jayval254 <valariekimuge1@gmail.com>
2024-06-10 10:21:31 -0700

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Function that calculates the min operations to copy and paste letters
+"""
+
+
+def minOperations(n):
+"""calculates the fewest number of operations
+    needed to result in exactly n H characters
+    in the file"""
+
+    nOp = 0
+    minOp = 2
+    while n > 1:
+        while n % minOp == 0:
+            nOp += minOp
+            n /= minOp
+        minOp += 1
+    return nOp
